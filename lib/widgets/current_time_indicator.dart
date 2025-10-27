b484794ac0f3347180e90cd2b07a21e2f776b71f
carolinamaio123 <carolinamaio123@gmail.com>
2025-10-27 03:44:46 +0000

    Indicador do tempo actual

---
+++ lib/widgets/current_time_indicator.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../../utils/time_utils.dart';
+
+class CurrentTimeIndicator extends StatelessWidget {
+  const CurrentTimeIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final now = TimeOfDay.now();
+    final topOffset = TimeUtils.timeToOffset(now);
+
+    return Positioned(
+      top: topOffset,
+      left: 0,
+      right: 0,
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 60,
+            child: Text(
+              'Agora',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.redAccent, fontSize: 10, fontWeight: FontWeight.w600),
+            ),
+          ),
+          Expanded(child: Container(height: 1.2, color: Colors.redAccent)),
+          const Padding(
+            padding: EdgeInsets.only(left: 10),
+            child: CircleAvatar(radius: 5, backgroundColor: Colors.redAccent),
+          ),
+        ],
+      ),
+    );
+  }
+}
