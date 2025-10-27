3aa01c037cf0fe1523dfb398622ac051f37b0e86
carolinamaio123 <carolinamaio123@gmail.com>
2025-10-27 04:28:24 +0000

    Melhorias na apresentação
    Refactor TimeColumn to use StatefulWidget for dynamic time updates.

--- lib/widgets/time_column.dart
+++ lib/widgets/time_column.dart
@@ -1,44 +1,87 @@
 import 'package:flutter/material.dart';
 import '../utils/time_utils.dart';
 
-class TimeColumn extends StatelessWidget {
+class TimeColumn extends StatefulWidget {
   const TimeColumn({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      color: Colors.grey.shade100,
-      child: ListView.builder(
-        itemCount: (TimeUtils.endHour - TimeUtils.startHour) * 4,
-        itemBuilder: (context, index) {
-          final totalMinutes = index * 15;
-          final hour = totalMinutes ~/ 60;
-          final minute = totalMinutes % 60;
-          final isHour = minute == 0;
-          final isHalf = minute == 30;
+  State<TimeColumn> createState() => _TimeColumnState();
+}
 
-          return Container(
-            height: TimeUtils.quarterHeight,
-            alignment: Alignment.topCenter,
-            decoration: BoxDecoration(
-              border: Border(
-                top: BorderSide(
-                  color: isHalf ? Colors.grey.shade600 : Colors.grey.shade300,
-                  width: isHalf ? 1.2 : 0.6,
+class _TimeColumnState extends State<TimeColumn> with SingleTickerProviderStateMixin {
+  late TimeOfDay _now;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _now = TimeOfDay.now();
+
+    // Atualiza a hora a cada segundo para mover a linha suavemente
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) {
+      setState(() {
+        _now = TimeOfDay.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final totalQuarters = (TimeUtils.endHour - TimeUtils.startHour) * 4;
+    final columnWidth = 50.0;
+
+    return Stack(
+      children: [
+        Column(
+          children: List.generate(totalQuarters, (i) {
+            final hour = TimeUtils.startHour + (i ~/ 4);
+            final minute = (i % 4) * 15;
+
+            return Container(
+              height: TimeUtils.quarterHeight,
+              width: columnWidth,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: i % 8 < 4 ? Colors.grey.shade100 : Colors.white,
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.shade300, width: 0.5),
                 ),
               ),
-            ),
-            child: isHour
-                ? Padding(
-                    padding: const EdgeInsets.only(top: 2.0),
-                    child: Text('${hour.toString().padLeft(2, '0')}h',
-                        style: TextStyle(fontSize: 11, color: Colors.grey.shade700)),
-                  )
-                : null,
-          );
-        },
-      ),
+              child: Text(
+                '${hour.toString().padLeft(2, '0')}:${minute.toString().padLeft(2, '0')}',
+                style: TextStyle(
+                  fontSize: 11,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blueGrey.shade700,
+                ),
+              ),
+            );
+          }),
+        ),
+        // Linha vermelha indicando a hora atual
+        Positioned(
+          top: _calculateOffset(_now),
+          left: 0,
+          right: 0,
+          child: Container(
+            height: 2,
+            color: Colors.redAccent,
+          ),
+        ),
+      ],
     );
+  }
+
+  double _calculateOffset(TimeOfDay now) {
+    // Total de minutos desde a hora de início
+    final totalMinutes = (now.hour - TimeUtils.startHour) * 60 + now.minute;
+    // Converte para altura em pixels
+    return totalMinutes / 15 * TimeUtils.quarterHeight;
   }
 }
