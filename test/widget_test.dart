086cfacc3600a81b45699297950ae23d3e9608da
andre <carolinamaio123@gmail.com>
2025-10-27 13:27:00 +0000

    correçao do widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,9 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:turnos_diarios/main.dart'; // muda para o teu package/main.dart
+import 'package:turnos_diarios/main.dart';
 
 void main() {
   testWidgets('Smoke test mínimo', (WidgetTester tester) async {
-    // Carrega a app
-    await tester.pumpWidget(const MyApp());
-
-    // Apenas verifica que o widget inicial existe
-    expect(find.byType(MyApp), findsOneWidget);
+    await tester.pumpWidget(const ScheduleApp());
+    expect(find.byType(ScheduleApp), findsOneWidget);
   });
 }
